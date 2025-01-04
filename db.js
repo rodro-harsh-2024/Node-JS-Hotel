5cd964eea0f6a5bf436ddc0e99b08839ce9aae55
Harsh <harsh.thakur060301@gmail.com>
2025-01-04 16:29:44 +0530

    ok

--- db.js
+++ db.js
@@ -5,10 +5,13 @@ require('dotenv').config();
 // const mongoURL = process.env.MONGO_DB_URL_LOCAL;
 const mongoURL = process.env.DB_URL;
 
-mongoose.connect(mongoURL,{
-    useNewUrlParser : true,
-    useUnifiedTopology: true
-});
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    ssl: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
 const db = mongoose.connection;
 
